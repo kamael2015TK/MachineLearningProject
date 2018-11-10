61a4d83539f052c78bee7981376eb1a432dab266
Taras Karpin S153067 <tarass777@hotmail.com>
2018-11-10 14:31:06 +0100

    Removed not needed variables, renamed some variables

--- src/regression.py
+++ src/regression.py
@@ -1,8 +1,8 @@
 from standardizer import standardize
 
-
 #run file reader we will get X and lables
 from file_reader import *
 
 # get standardized data
-standardizedData = standardize(X)
+standardizedData = standardize(data)
+
